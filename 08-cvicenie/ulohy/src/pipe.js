5509a93dca149e4b9770926a44218d89858a64f5
Juraj Mecir <jurajmecir@gmail.com>
2019-11-17 20:36:22 +0100

    du cviko 8

--- 08-cvicenie/ulohy/src/pipe.js
+++ 08-cvicenie/ulohy/src/pipe.js
@@ -1,16 +1,20 @@
 // generic composition
 /*
-const pipe = (...fns) =>
-  x => fns.reduce((v, f) => f(v), x);
+
 */
 
 function pipe(...fns) {
   // TODO: rewrite above reduce to for cycle
-  // urcite to bude vracat funkciu 
+  // urcite to bude vracat funkciu
   // len ju nejako musite poskladat
   // ako ? musite vediet precitat ten reduce hore
-  let r=function(){};
-  return r;
+return function (a) {
+    let b = a
+    for (let fn of fns) {
+      b = fn(b)
+    }
+    return b
+  };
 }
 module.exports = pipe;
 
@@ -22,9 +26,9 @@ process.env.SELF_TEST && (() => {
   
   const assert = require("assert");
 
-  const a = (v) => `a(${v})`
-  const b = (v) => `b(${v})`
-  const c = (v) => `c(${v})`
+  const a = (v) => a(`${v}`)
+  const b = (v) => b(`${v}`)
+  const c = (v) => c(`${v}`)
 
   assert.equal(pipe(a, b, c)("x"), "c(b(a(x)))");
 
@@ -32,4 +36,3 @@ process.env.SELF_TEST && (() => {
 
   console.error(`[self test]:${__filename}:OK`)
 })();
-
